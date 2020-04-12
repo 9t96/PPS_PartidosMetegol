52ce9ee0c6ed16f49b8b90d04141d88fddf997c3
laloo96 <ohmycaio@gmail.com>
2020-04-12 15:59:51 -0300

    -Login funcionando contra firebase
    -Agregado auth service
    -routing base

--- PartidosMetegol/src/environments/environment.prod.ts
+++ PartidosMetegol/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC7EWdqIweG4-xv0LIzTttYRyES3CcJ1nQ",
+    authDomain: "partidosmetegol-1c327.firebaseapp.com",
+    databaseURL: "https://partidosmetegol-1c327.firebaseio.com",
+    projectId: "partidosmetegol-1c327",
+    storageBucket: "partidosmetegol-1c327.appspot.com",
+    messagingSenderId: "940885471440",
+    appId: "1:940885471440:web:0a85cd04dd608d44606ed5",
+    measurementId: "G-QXG6CR58P3"
+  }
 };
--- PartidosMetegol/src/environments/environment.ts
+++ PartidosMetegol/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC7EWdqIweG4-xv0LIzTttYRyES3CcJ1nQ",
+    authDomain: "partidosmetegol-1c327.firebaseapp.com",
+    databaseURL: "https://partidosmetegol-1c327.firebaseio.com",
+    projectId: "partidosmetegol-1c327",
+    storageBucket: "partidosmetegol-1c327.appspot.com",
+    messagingSenderId: "940885471440",
+    appId: "1:940885471440:web:0a85cd04dd608d44606ed5",
+    measurementId: "G-QXG6CR58P3"
+  }
 };
 
 /*
